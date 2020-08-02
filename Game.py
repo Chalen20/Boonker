43defcc12b149f83b7ed195e08f61a5531687f02
Andrii Chaliuk <0632135592an@gmail.com>
2020-08-02 14:03:58 +0300

    commit40

--- Game.py
+++ Game.py
@@ -282,26 +282,26 @@ class Game:
                                     de[1] + self.width_rubashka / 8 * i + self.width_rubashka / 16,
                                     text=str(self.pers_cards[number - 1][2]) + ", " +
                                          str(self.pers_cards[number - 1][3]),
-                                    anchor="c", font=("Verdana", 20), tag=texts[i] + str(number))
+                                    anchor="c", font=("Verdana", 15), tag=texts[i] + str(number))
 
         elif texts[i] == "add_info":
             self.canvas.create_text(de[0] + self.width_rubashka * 0.35,
                                     de[1] + self.width_rubashka / 8 * i + self.width_rubashka / 16,
                                     text=str(self.pers_cards[number - 1][4]),
-                                    anchor="c", font=("Verdana", 20), tag=texts[i] + str(number))
+                                    anchor="c", font=("Verdana", 15), tag=texts[i] + str(number))
 
         elif texts[i] == "human_trait":
             self.canvas.create_text(de[0] + self.width_rubashka * 0.35,
                                     de[1] + self.width_rubashka / 8 * i + self.width_rubashka / 16,
                                     text=str(self.pers_cards[number - 1][5]) + ", " +
                                          str(self.pers_cards[number - 1][6]),
-                                    anchor="c", font=("Verdana", 20), tag=texts[i] + str(number))
+                                    anchor="c", font=("Verdana", 15), tag=texts[i] + str(number))
 
         elif texts[i] == "phobia":
             self.canvas.create_text(de[0] + self.width_rubashka * 0.35,
                                     de[1] + self.width_rubashka / 8 * i + self.width_rubashka / 16,
                                     text=str(self.pers_cards[number - 1][7]),
-                                    anchor="c", font=("Verdana", 20), tag=texts[i] + str(number))
+                                    anchor="c", font=("Verdana", 15), tag=texts[i] + str(number))
 
         elif texts[i] == "biological":
             self.canvas.create_text(de[0] + self.width_rubashka * 0.35,
@@ -309,12 +309,12 @@ class Game:
                                     text=str(self.pers_cards[number - 1][8]) + ", " + str(
                                         self.pers_cards[number - 1][9])
                                          + ", " + str(self.pers_cards[number - 1][10]),
-                                    anchor="c", font=("Verdana", 20), tag=texts[i] + str(number))
+                                    anchor="c", font=("Verdana", 15), tag=texts[i] + str(number))
         elif texts[i] == "health":
             self.canvas.create_text(de[0] + self.width_rubashka * 0.35,
                                     de[1] + self.width_rubashka / 8 * i + self.width_rubashka / 16,
                                     text=str(self.pers_cards[number - 1][11]),
-                                    anchor="c", font=("Verdana", 20), tag=texts[i] + str(number))
+                                    anchor="c", font=("Verdana", 15), tag=texts[i] + str(number))
 
         elif texts[i] == "body_type":
             self.canvas.create_text(de[0] + self.width_rubashka * 0.35,
@@ -322,7 +322,7 @@ class Game:
                                     text=str(
                                         self.pers_cards[number - 1][12] + ", " + str(self.pers_cards[number - 1][13])
                                         + ", " + str(self.pers_cards[number - 1][14])),
-                                    anchor="c", font=("Verdana", 20), tag=texts[i] + str(number))
+                                    anchor="c", font=("Verdana", 15), tag=texts[i] + str(number))
 
     def job_func(self, de, number, event):
         number = number - 1
@@ -336,7 +336,7 @@ class Game:
         text = str(self.pers_cards[number][0]) + ", " + str(self.pers_cards[number][1]) + " " + years
         if len(text) >= 22:
             for i in range(len(text)-1, 0, -1):
-                if text[i] == " ":
+                if text[i] == ",":
                     text = text[:i] + "\n" + text[i:]
                     self.canvas.create_text(de[0] + self.width_rubashka * 0.7 / 2, de[1] + self.width_rubashka / 8 / 2.5,
                                             text=text,
@@ -407,39 +407,3 @@ class Timer:
                                               font=("Verdana", 20), anchor='nw', fill=self.color_text)
         if self.seconds != 0 or self.minutes != 0:
             self.root.after(100, self.time)
-
-
-Game(player_count)
-
-class Saver:
-    def __init__(self, player_number, job, hobby, add_info, human_trait, phobia, biological, health, body_type):
-        f = open('players.html', 'w')
-        message = """<html>
-        <head>
-                <style> .card {
-                  margin : auto ;
-                  width : 400px;
-                  height : 600px;
-                  background-color: #f7d065;
-                  border-radius: 40px;}
-                  p {
-                  padding : 20px 0 0 25px
-                  }
-</style>
-        </head>
-<body>
-  <div class = "card">
-          <p>""" + player_number + """</p>
-          <p>""" + job + """</p>
-          <p>""" + hobby + """</p>
-          <p>""" + add_info + """</p>
-          <p>""" + human_trait + """</p>
-          <p>""" + phobia + """</p>
-          <p>""" + biological + """</p>
-          <p>""" + health + """</p>
-          <p>""" + body_type + """</p>
-  </div></body>
-</html>"""
-
-        f.write(message)
-        f.close()
